33608d1c9c01e7638ed29608e305af6f95fd5fcd
Denniscc <tonynooy@gmail.com>
2021-01-29 20:23:03 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -3,30 +3,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
-const mongoose = require('mongoose')
 const Restaurant = require('./models/restaurant')
 const bodyParser = require('body-parser')
 // 載入 method-override
 const methodOverride = require('method-override')
 // 引用路由器
 const routes = require('./routes')
+require('./config/mongoose')
 
 // 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
 app.use(bodyParser.urlencoded({ extended: true }))
-
-// 設定連線到 mongoDB
-mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true })
-
-// 取得資料庫連線狀態
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-// 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
 
 //設定handlebars引擎
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
